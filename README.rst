6c8d1fd7d9a33b1e4e09d3f7ae1d971eaab106c5
slimkrazy <samuel.adu@gmail.com>
2012-01-01 21:26:56 +0000

    Make detailed data fetching more explicit, as it requires an additional API call.

--- README.rst
+++ README.rst
@@ -15,10 +15,18 @@ Installation
 sudo pip install https://github.com/slimkrazy/python-google-places/zipball/master
 
 
+Prerequisites
+============
+
+.. _prerequisites:
+A Google API key with Places activated against it. Please check the Google API
+console, here: http://code.google.com/apis/console
+
+
 Usage
 =====
 
-.. _flavours:
+.. _usage:
 Code is easier to understand than words, so::
 
     from googleplaces import GooglePlaces, types
@@ -32,12 +40,68 @@ Code is easier to understand than words, so::
     if query_result.has_attributions:
         print query_result.html_attributions
 
+
     for place in query_result.places:
+	    # Returned places from a query are place summaries.
         print place.name
         print place.geo_location
         print place.reference
 
 
+
+
+Known Issues
+=========
+Support for adding and deleting pending.
+
+
 Reference
 =========
-TODO
+googleplaces.geocode_location(location, sensor=False)
+
+
+googleplaces.GooglePlacesError
+googleplaces.GooglePlacesAttributeError
+
+
+googleplaces.GooglePlaces
+  query()
+    Returns googleplaces.GooglePlacesSearchResult
+
+  get_place(reference)
+  Returns a detailed instance of googleplaces.Place
+
+  checkin(reference, sensor=False)
+
+
+googleplaces.GooglePlacesSearchResult
+  places
+    A list of summary googleplaces.Place instances.
+
+  has_attributions()
+
+  html_attributions()
+
+
+googleplaces.Place
+  reference
+  id
+  icon
+  types
+  geo_location
+  name
+  vicinity
+  rating
+
+  details
+  formatted_address
+  local_phone_number
+  international_phone_number
+  website
+  url
+
+  has_attributions()
+  html_attributions()
+  checkin()
+  get_details()
+
--- googleplaces/__init__.py
+++ googleplaces/__init__.py
@@ -23,9 +23,9 @@ import urllib2
 import types
 
 
-__all__ = ['GooglePlaces', 'GooglePlacesError', 'geocode_location',
-           'checkin_place']
-__version__ = '0.0.1'
+__all__ = ['GooglePlaces', 'GooglePlacesError', 'GooglePlacesAttributeError',
+           'geocode_location']
+__version__ = '0.8.0'
 __author__ = 'Samuel Adu'
 __email__ = 'sam@slimkrazy.com'
 
@@ -67,25 +67,18 @@ def geocode_location(location, sensor=False):
         raise GooglePlacesError, error_detail
     return geo_response['results'][0]['geometry']['location']
 
-def checkin(reference, api_key, sensor=False):
-    """Checks in a user to a place.
+def _get_place_details(reference, api_key, sensor=False):
+    """Gets a detailed place response.
 
     keyword arguments:
-    reference -- The unique  Google reference for the relevant place.
-    api_key   -- Your Google API key
-    sensor    -- Boolean flag denoting if the location came from a device using
-                 its' location sensor (default False)
+    reference -- The unique Google reference for the required place.
     """
-
-    data = {'reference': reference}
-    url, checkin_response = _fetch_remote_json(
-            GooglePlaces.CHECKIN_API_URL % (str(sensor).lower(), api_key),
-            json.dumps(data), use_http_post=True)
-    _validate_response(url, checkin_response)
-
-def get_detailed_object(reference):
-    # TODO - Implementation.
-    pass
+    url, detail_response = _fetch_remote_json(GooglePlaces.DETAIL_API_URL,
+                                              {'reference': reference,
+                                               'sensor': str(sensor).lower(),
+                                               'key': api_key})
+    _validate_response(url, detail_response)
+    return detail_response['result']
 
 def _validate_response(url, response):
     """Validates that the response from Google was successful."""
@@ -97,6 +90,24 @@ def _validate_response(url, response):
 
 
 class GooglePlacesError(Exception):
+    pass
+
+
+class GooglePlacesAttributeError(AttributeError):
+    """Exception thrown when a detailed property is unavailable.
+
+    A search query from the places API returns only a summary of the Place.
+    in order to get full details, a further API call must be made using
+    the place reference. This exception will be thrown when a property made
+    available by only the detailed API call is looked up against the summary
+    object.
+
+    An explicit call to get_details() must be made on the summary object in
+    order to convert a summary object to a detailed object.
+    """
+    # I could spend forever muling between this design decision and creating
+    # a PlaceSummary object as well as a Place object. I'm leaning towards this
+    # method in order to keep the API as simple as possible.
     pass
 
 
@@ -159,6 +170,31 @@ class GooglePlaces(object):
         _validate_response(url, places_response)
         return GooglePlacesSearchResult(self, places_response)
 
+    def checkin(self, reference, sensor=False):
+        """Checks in a user to a place.
+
+        keyword arguments:
+        reference -- The unique Google reference for the relevant place.
+        sensor    -- Boolean flag denoting if the location came from a
+                         device using its' location sensor (default False)
+        """
+        data = {'reference': reference}
+        url, checkin_response = _fetch_remote_json(
+                GooglePlaces.CHECKIN_API_URL % (str(sensor).lower(),
+                        self.api_key), json.dumps(data), use_http_post=True)
+        _validate_response(url, checkin_response)
+
+    def get_place(self, reference, sensor=False):
+        """Gets a detailed place object.
+
+        keyword arguments:
+        reference -- The unique Google reference for the required place.
+        sensor    -- Boolean flag denoting if the location came from a
+                         device using its' location sensor (default False)
+        """
+        place_details = _get_place_details(reference, self.api_key, sensor)
+        return Place(self, place_details)
+
     def _add_required_param_keys(self):
         self._request_params['key'] = self.api_key
         self._request_params['sensor'] = str(self.sensor).lower()
@@ -210,58 +246,52 @@ class Place(object):
     Represents a place from the results of a Google Places API query.
     """
     def __init__(self, query_instance, place_data):
-        self._query_api_key = query_instance.api_key
-        self._query_sensor = query_instance.sensor
+        self._query_instance = query_instance
         self._id = place_data['id']
         self._reference = place_data['reference']
         self._name = place_data['name']
         self._vicinity = place_data['vicinity']
         self._geo_location = place_data['geometry']['location']
         self._rating = place_data.get('rating')
-        self._details = None
-
-    @property
-    def details(self):
-        """Returns the JSON response from Google Places Detail search API."""
-        self._set_details()
-        return self._details
-
-    @property
-    def id(self):
-        return self._id
+        self._types = place_data.get('types')
+        self._icon = place_data.get('icon')
+        if place_data.get('address_components') is None:
+            self._details = None
+        else:
+            self._details = place_data
 
     @property
     def reference(self):
-        """Returns the unique Google reference associated to the place."""
+        """Returns contains a unique token for the place.
+
+        The token can be used to retrieve additional information about this
+        place when invoking the getPlace method on an GooglePlaces instance.
+
+        You can store this token and use it at any time in future to refresh
+        cached data about this Place, but the same token is not guaranteed to
+        be returned for any given Place across different searches."""
         return self._reference
 
     @property
-    def name(self):
-        """Returns the name of the place."""
-        return self._name
+    def id(self):
+        """Returns the unique stable identifier denoting this place.
+
+        This identifier may not be used to retrieve information about this
+        place, but is guaranteed to be valid across sessions. It can be used
+        to consolidate data about this Place, and to verify the identity of a
+        Place across separate searches.
+        """
+        return self._id
 
     @property
-    def vicinity(self):
-        return self._vicinity
+    def icon(self):
+        """Returns the URL of a recommended icon for display."""
+        return self._icon
 
     @property
-    def rating(self):
-        return self._rating
-
-    @property
-    def formatted_address(self):
-        self._set_details()
-        return self.details.get('formatted_address')
-
-    @property
-    def local_phone_number(self):
-        self._set_details()
-        return self.details.get('formatted_phone_number')
-
-    @property
-    def international_phone_number(self):
-        self._set_details()
-        return self.details.get('international_phone_number')
+    def types(self):
+        """Returns a list of feature types describing the given result."""
+        return self._types
 
     @property
     def geo_location(self):
@@ -272,6 +302,75 @@ class Place(object):
         return self._geo_location
 
     @property
+    def name(self):
+        """Returns the human-readable name of the place."""
+        return self._name
+
+    @property
+    def vicinity(self):
+        """Returns a feature name of a nearby location.
+
+        Often this feature refers to a street or neighborhood within the given
+        results.
+        """
+        return self._vicinity
+
+    @property
+    def rating(self):
+        """Returns the Place's rating, from 0.0 to 5.0, based on user reviews.
+
+        This method will return None for places that have no rating.
+        """
+        return self._rating
+
+    # The following properties require a further API call in order to be
+    # available.
+    @property
+    def details(self):
+        """Returns the JSON response from Google Places Detail search API."""
+        self._validate_status()
+        return self._details
+
+    @property
+    def formatted_address(self):
+        """Returns a string containing the human-readable address of this place.
+
+        Often this address is equivalent to the "postal address," which
+        sometimes differs from country to country. (Note that some countries,
+        such as the United Kingdom, do not allow distribution of complete postal
+        addresses due to licensing restrictions.)
+        """
+        self._validate_status()
+        return self.details.get('formatted_address')
+
+    @property
+    def local_phone_number(self):
+        """Returns the Place's phone number in its local format."""
+        self._validate_status()
+        return self.details.get('formatted_phone_number')
+
+    @property
+    def international_phone_number(self):
+        self._validate_status()
+        return self.details.get('international_phone_number')
+
+    @property
+    def website(self):
+        """Retuns the authoritative website for this Place."""
+        self._validate_status()
+        return self.details.get('website')
+
+    @property
+    def url(self):
+        """Contains the official Google Place Page URL of this establishment.
+
+        Applications must link to or embed the Google Place page on any screen
+        that shows detailed results about this Place to the user.
+        """
+        self._validate_status()
+        return self.details.get('url')
+
+    @property
     def html_attributions(self):
         """Returns the HTML attributions for the specified response.
 
@@ -279,25 +378,33 @@ class Place(object):
         with the requirements as found in the documentation. Please see the
         module comments for links to the relevant url.
         """
-        self._set_details()
+        self._validate_status()
         return self.details.get('html_attributions', [])
 
     @property
     def has_attributions(self):
         """Returns a flag denoting if the response had any html attributions."""
-        return len(self.html_attributions) > 0
+        return (False if self._details is None else
+                len(self.html_attributions) > 0)
 
     def checkin(self):
         """Checks in an anonynomous user in."""
-        checkin(self.reference, self._query_api_key, self._query_sensor)
+        self._query_instance.checkin(self.reference,
+                                     self._query_instance.sensor)
 
-    def _set_details(self):
-        """Retrieves full information on the place matching the reference."""
+    def get_details(self):
+        """Retrieves full information on the place matching the reference.
+
+        Further attributes will be made available on the instance once this
+        method has been invoked.
+        """
         if self._details is None:
-            url, detail_response = _fetch_remote_json(
-                    GooglePlaces.DETAIL_API_URL,
-                    {'reference': self.reference,
-                            'sensor': str(self._query_sensor).lower(),
-                            'key': self._query_api_key})
-            _validate_response(url, detail_response)
-            self._details = detail_response['result']
+            self._details = _get_place_details(
+                    self.reference, self._query_instance.api_key,
+                    self._query_instance.sensor)
+
+    def _validate_status(self):
+        if self._details is None:
+            error_detail = ('The attribute requested is only available after ' +
+                    'an explicit call to get_details() is made.')
+            raise GooglePlacesAttributeError, error_detail
