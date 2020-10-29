1ddb7aa35983e845ede912f8cc415b115ef4d8be
Samuel Adu <samuel.adu@gmail.com>
2020-10-29 12:08:23 +0000

    Update __init__.py
    * Use .get() for accessing deprecated id field
    * Patch version bump

--- googleplaces/__init__.py
+++ googleplaces/__init__.py
@@ -35,7 +35,7 @@ from . import ranking
 
 __all__ = ['GooglePlaces', 'GooglePlacesError', 'GooglePlacesAttributeError',
            'geocode_location']
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __author__ = 'Samuel Adu'
 __email__ = 'sam@slimkrazy.com'
 
